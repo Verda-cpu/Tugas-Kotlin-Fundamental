547009e44e1f875c34ba283521c57f0c3fbaf2ae
Verda-cpu <verdazhafif@email.com>
2020-08-31 10:10:02 +0700

    first commit

---
+++ src/main/kotlin/HelloKotlin.kt
@@ -0,0 +1,11 @@
+fun main () {
+    val name = "M.Verda Zhafif.Z"
+    val hobi = "Main game dan Memancing"
+
+    print("Halo nama saya   :")
+    println(name)
+    print("Hobi saya adalah :")
+    println(hobi)
+    print(if (true)"always true" else "always false")
+
+}
